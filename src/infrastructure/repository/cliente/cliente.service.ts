66737a490a4ec380efac645c05a4ef3b0feaa7ed
whocastr <castro.luik@hotmail.com>
2023-07-13 11:04:20 -0300

    feat: implementação cliente service

--- src/infrastructure/repository/cliente/cliente.service.ts
+++ src/infrastructure/repository/cliente/cliente.service.ts
@@ -2,7 +2,7 @@ import { mapper } from '@/application/mapper/base.mapper';
 import { Cliente } from '@/domain/entity/cliente.model';
 import { IClienteService } from '@/domain/port/repository/cliente.interface';
 import { ClienteEntity } from '@/infrastructure/entity/cliente.entity';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm/repository/Repository';
 
@@ -20,19 +20,16 @@ export class ClienteService implements IClienteService {
 
   async findByCPF(cpf: string): Promise<Cliente> {
     const categorias = await this.clienteRepository.findOneBy({ cpf: cpf });
-
     return mapper.map(categorias, ClienteEntity, Cliente);
   }
 
   async save(cliente: Cliente): Promise<Cliente> {
     const resultado = await this.clienteRepository.save(cliente);
-
     return resultado;
   }
 
   async saveMany(cliente: Cliente[]): Promise<Cliente[]> {
     const resultado = await this.clienteRepository.save(cliente);
-
     return resultado;
   }
 
