79eeec489d725da27885701d5b8937fb9ccfbc31
Leonardo Paz <leonardocovelodapaz@gmail.com>
2023-08-27 17:14:11 -0300

    feat: tipando entradas e saidas para categoria

--- src/domain/port/usecase/categoria.usecase.ts
+++ src/domain/port/usecase/categoria.usecase.ts
@@ -1,49 +1,53 @@
 import { mapper } from '@/application/mapper/base.mapper';
 import { Categoria } from '@/domain/entity/categoria.model';
-import { Cliente } from '@/domain/entity/cliente.model';
 import { ErroNegocio } from '@/domain/exception/erro.module';
+import {
+  AdicionarCategoriaInput,
+  AdicionarCategoriaOutput,
+} from '@/infrastructure/dto/categoria/adicionarCategoria.dto';
+import {
+  AtualizarCategoriaPorIdInput,
+  AtualizarCategoriaPorIdOutput,
+} from '@/infrastructure/dto/categoria/atualizarCategoriaPorId.dto';
+import { ObterCategoriaPorIdOutput } from '@/infrastructure/dto/categoria/obterCategoriaPorId.dto';
+import { ObterCategoriasOutput } from '@/infrastructure/dto/categoria/obterCategorias.dto';
 import { CategoriaService } from '@/infrastructure/repository/categoria/categoria.service';
-import { ClienteService } from '@/infrastructure/repository/cliente/cliente.service';
-import { AutoMap } from '@automapper/classes';
-import { createMap } from '@automapper/core';
 import { Injectable } from '@nestjs/common';
-
-class Input {
-  @AutoMap()
-  descricao: string;
-}
-
-class Output {
-  @AutoMap()
-  id: string;
-
-  @AutoMap()
-  descricao: string;
-}
-
-createMap(mapper, Input, Categoria);
-createMap(mapper, Categoria, Output);
 
 @Injectable()
 export class CategoriaUseCase {
   constructor(private categoriaService: CategoriaService) {}
 
-  async adicionarCategoria(input: Input): Promise<Output> {
-    const categoria: Categoria = mapper.map(input, Input, Categoria);
+  async adicionarCategoria(
+    input: AdicionarCategoriaInput,
+  ): Promise<AdicionarCategoriaOutput> {
+    const categoria: Categoria = mapper.map(
+      input,
+      AdicionarCategoriaInput,
+      Categoria,
+    );
 
     categoria.validarCategoria();
 
     const categoriaAdicionada = await this.categoriaService.save(categoria);
 
-    return mapper.map(categoriaAdicionada, Categoria, Output);
+    return mapper.map(categoriaAdicionada, Categoria, AdicionarCategoriaOutput);
   }
 
   async removerCategoriaPorId(id: string): Promise<void> {
     await this.categoriaService.remove(id);
   }
 
-  async atualizarCategoriaPorId(input: Input): Promise<Output> {
-    const categoria: Categoria = mapper.map(input, Input, Categoria);
+  async atualizarCategoriaPorId(
+    id: string,
+    input: AtualizarCategoriaPorIdInput,
+  ): Promise<AtualizarCategoriaPorIdOutput> {
+    const categoria: Categoria = mapper.map(
+      input,
+      AtualizarCategoriaPorIdInput,
+      Categoria,
+    );
+    categoria.id = id;
 
     categoria.validarCategoria();
 
@@ -56,22 +60,26 @@ export class CategoriaUseCase {
     categoria.id = categoriaExiste.id;
     const categoriaAtualizada = await this.categoriaService.save(categoria);
 
-    return mapper.map(categoriaAtualizada, Categoria, Output);
+    return mapper.map(
+      categoriaAtualizada,
+      Categoria,
+      AtualizarCategoriaPorIdOutput,
+    );
   }
 
-  async obterCategoriaPorId(id: string): Promise<Output> {
+  async obterCategoriaPorId(id: string): Promise<ObterCategoriaPorIdOutput> {
     const categoria = await this.categoriaService.findById(id);
 
     if (!categoria) {
       throw new ErroNegocio('cliente-nao-cadastrado');
     }
 
-    return mapper.map(categoria, Categoria, Output);
+    return mapper.map(categoria, Categoria, ObterCategoriaPorIdOutput);
   }
 
-  async obterCategorias(): Promise<Output[]> {
+  async obterCategorias(): Promise<ObterCategoriasOutput[]> {
     const categorias = await this.categoriaService.find();
 
-    return mapper.mapArray(categorias, Categoria, Output);
+    return mapper.mapArray(categorias, Categoria, ObterCategoriasOutput);
   }
 }
