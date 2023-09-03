4f15148b51fd04d78388203dae154a1341b181cd
whocastr <castro.luik@hotmail.com>
2023-09-03 11:53:56 -0300

    feat: configuração para obter .env da secret

--- src/infrastructure/repository/helper/connection.module.ts
+++ src/infrastructure/repository/helper/connection.module.ts
@@ -11,11 +11,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '127.0.0.1',
-      username: 'root',
-      password: 'root',
-      database: 'fiap',
-      port: 3306,
+      host: process.env.DATABASE_HOST,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_SCHEMA,
+      port: Number(process.env.DATABASE_PORT),
       entities: [
         CategoriaEntity,
         ClienteEntity,
